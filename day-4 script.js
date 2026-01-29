80a94ab4d912c89c9218905489d3544845e73655
manjeet417 <deswalm747@gmail.com>
2026-01-29 10:51:26 +0530

    update

--- day-4 script.js
+++ day-4 script.js
@@ -1,7 +1,36 @@
 let user={
     fullname:"manjeet",
-    address:"gurugram",
+    address:{
+        city:"gurugram",
+        state:"hariyana"
+    },
     mobileno:"1234567890",
-    favcolor:["black","red","white"]
-}
-console.log(user);
+    favcolor:["black","red","white"],
+    demo : function(){
+        return "demo function";
+    },
+};
+console.log(
+    user.fullname,
+    user.mobileno,
+    user.favcolor[1],
+    user.demo(),    
+    user.address.city,
+);
+
+
+
+const car = {
+    make: "mahindra",
+    model: "thar"
+};
+// we can't add a new key and a new value pair but we can change or update existing values 
+Object.seal(car);
+
+// we can't add a new key and value pair also can't change or update existing value 
+Object.freeze(car);
+
+car.model = "xuv700"
+car.color = "black"
+console.log(car);
+    
